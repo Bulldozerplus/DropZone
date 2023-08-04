5f3ff5f828d37e45ce3ee7f9e0c29ae152e26385
Nikita T <nikitataldykin3156@yandex.ru>
2023-08-04 11:25:40 +0300

    Realized dropZone

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [files, setFiles] = useState([])
+
+    const [answerLoad, setAnswerLoad] = useState('')
+
+    const ANSWERS = {
+        success: 'load complete',
+        bigName: 'To big name file',
+        bigSize: 'To big size file',
+        wrongType: 'Wrong type file'
+    }
+
+    function handleDrop(e) {
+        console.log(e.dataTransfer.files[0])
+
+        const sizeFile = e.dataTransfer.files[0].size
+        const nameFile = e.dataTransfer.files[0].name
+
+        const MAX_size = 5120
+
+
+        if (nameFile) {
+            if (!nameFile.includes('.docx', 'pdf', 'txt')) {
+                setAnswerLoad(ANSWERS.wrongType)
+            } else if (nameFile.length > 15) {
+                setAnswerLoad(ANSWERS.bigName)
+            }
+        } else if (sizeFile && sizeFile > MAX_size) {
+            setAnswerLoad(ANSWERS.bigSize)
+        } else {
+            setFiles(prevState => [...prevState, nameFile])
+            setAnswerLoad(ANSWERS.success)
+        }
+
+        e.stopPropagation()
+    }
+
+    return (
+        <div className="App">
+            <div className="dropzone" id="droptarget">
+                <input className='dropzone'
+                       type='file'
+                       value=""
+                       maxSize='5000'
+                       onDrop={handleDrop}/>Drop file here
+            </div>
+            <div>{files.length === 0
+                ? <h1>Files not fined</h1>
+                : files.map((file, index) => (
+                    <div>{index + 1}. {file}</div>
+                ))}
+            </div>
+            <div>{answerLoad}</div>
+        </div>
+    );
 }
+
 
 export default App;
