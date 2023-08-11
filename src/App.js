bc79e8404aca88adf175b4f75966699dc0516ac2
Nikita T <nikitataldykin3156@yandex.ru>
2023-08-11 16:41:28 +0300

    Realized dropZone

--- src/App.js
+++ src/App.js
@@ -3,109 +3,106 @@ import {useState} from "react";
 import {ANSWERS, MAX_size} from "./Const";
 
 function App() {
-    const [files, setFiles] = useState([])
+    const [fileList, setFileList] = useState([])
 
 
     function handleDrop(e) {
         const file = e.dataTransfer.files
         const sizeFile = e.dataTransfer.files[0].size
         const nameFile = e.dataTransfer.files[0].name
-        const spreadFile = [...file]
-
         const typeFile = nameFile.split('.').at(-1)
 
-        if (file) {
-            spreadFile.map(file => {
-                console.log(spreadFile)
-                if (file.size > MAX_size) {
-                    return setFiles(prevState => [...prevState, {
-                        file: file,
-                        name: nameFile,
-                        size: sizeFile,
-                        status: ANSWERS.bigSize
-                    }])
-                }
-                if (nameFile.length > 15) {
-                    return setFiles(prevState => [...prevState, {
-                        file: file,
-                        name: nameFile,
-                        size: sizeFile,
-                        status: ANSWERS.bigName
-                    }])
-                }
+        console.log(e.dataTransfer.files[0])
+        console.log(file)
 
-                if (typeFile !== '.docx' || '.txt') {
-                    return setFiles(prevState => [...prevState, {
-                        file: file,
-                        name: nameFile,
-                        size: sizeFile,
-                        status: ANSWERS.wrongType
-                    }])
+        if (file && file.length <= 1) {
+            if (file.size > MAX_size) {
+                return setFileList(prevState => [...prevState, {
+                    file: file,
+                    name: nameFile,
+                    size: sizeFile,
+                    status: ANSWERS.bigSize
+                }])
+            }
+
+            if (nameFile.length > 15) {
+                return setFileList(prevState => [...prevState, {
+                    file: file,
+                    name: nameFile,
+                    size: sizeFile,
+                    status: ANSWERS.bigName
+                }])
+            }
+
+            if (typeFile !== '.docx') {
+                return setFileList(prevState => [...prevState, {
+                    file: file,
+                    name: nameFile,
+                    size: sizeFile,
+                    status: ANSWERS.wrongType
+                }])
+            }
+
+            if (file && file.length > 1) {
+                for (const currentFile in file) {
+                    if (currentFile.size > MAX_size) {
+                        return setFileList(prevState => [...prevState, {
+                            file: file,
+                            name: nameFile,
+                            size: sizeFile,
+                            status: ANSWERS.bigSize
+                        }])
+                    }
+                    if (nameFile.length > 15) {
+                        return setFileList(prevState => [...prevState, {
+                            file: file,
+                            name: nameFile,
+                            size: sizeFile,
+                            status: ANSWERS.bigName
+                        }])
+                    }
+                    if (typeFile !== '.docx') {
+                        return setFileList(prevState => [...prevState, {
+                            file: file,
+                            name: nameFile,
+                            size: sizeFile,
+                            status: ANSWERS.wrongType
+                        }])
+                    }
                 }
-                return setFiles(prevState => [...prevState, {
+            }
+
+                return setFileList(prevState => [...prevState, {
                     file: file,
                     name: nameFile,
                     size: sizeFile,
                     status: ANSWERS.success
                 }])
 
-            })
-            console.log(file)
+            }
+
+
         }
 
 
-        // if (file) {
-        //     if (!nameFile.includes('.docx')) {
-        //         return setFiles(prevState => [...prevState, {
-        //             file: file,
-        //             name: nameFile,
-        //             size: sizeFile,
-        //             status: ANSWERS.wrongType
-        //         }])
-        //     } if (nameFile.length > 15) {
-        //        return setFiles(prevState => [...prevState, {
-        //             file: file,
-        //             name: nameFile,
-        //             size: sizeFile,
-        //             status: ANSWERS.bigName
-        //         }])
-        //     } if (sizeFile > MAX_size) {
-        //         return setFiles(prevState => [...prevState, {
-        //             file: file,
-        //             name: nameFile,
-        //             size: sizeFile,
-        //             status: ANSWERS.bigSize
-        //         }])
-        //     }
-        //         return setFiles(prevState => [...prevState, {
-        //             file: file,
-        //             name: nameFile,
-        //             size: sizeFile,
-        //             status: ANSWERS.success
-        //         }])
-        // }
-
+        return (
+            <div className="App">
+                <div className="dropzone" id="droptarget">
+                    <input className='dropzone'
+                           type='file'
+                           multiple
+                           value=""
+                           onDrop={handleDrop}/>Drop file here
+                </div>
+                <div>{fileList.length === 0
+                    ? <h1>Files not fined</h1>
+                    : fileList.map((file, index) => (
+                        <div>{index + 1}. <strong>{file.name}</strong>, {file.size} - {file.status}</div>
+                    ))}
+                </div>
+            </div>
+        );
     }
 
 
-    return (
-        <div className="App">
-            <div className="dropzone" id="droptarget">
-                <input className='dropzone'
-                       type='file'
-                       multiple
-                       value=""
-                       onDrop={handleDrop}/>Drop file here
-            </div>
-            <div>{files.length === 0
-                ? <h1>Files not fined</h1>
-                : files.map((file, index) => (
-                    <div>{index + 1}. <strong>{file.name}</strong>, {file.size} - {file.status}</div>
-                ))}
-            </div>
-        </div>
-    );
-}
-
-
-export default App;
+    export default App;
