bd1535f8b4dcb49f017c9e86296f4be4297b3d37
Nikita T <nikitataldykin3156@yandex.ru>
2023-08-12 13:08:36 +0300

    Realized dropZone

--- src/App.js
+++ src/App.js
@@ -32,76 +32,59 @@ function App() {
                 }])
             }
 
-            if (typeFile !== '.docx') {
-                return setFileList(prevState => [...prevState, {
-                    file: file,
-                    name: nameFile,
-                    size: sizeFile,
-                    status: ANSWERS.wrongType
-                }])
-            }
 
-            else if(file && file.length > 1) {
-                const arrFileList = [...file]
-                arrFileList.map(currentFile => {
-                    if (currentFile.size > MAX_size) {
+            return setFileList(prevState => [...prevState, {
+                file: file,
+                name: nameFile,
+                size: sizeFile,
+                status: ANSWERS.success
+            }])
+
+        } else if (file.length > 1) {
+            const arrFileList = [...file]
+            arrFileList.map(currentFile => {
+                console.log(currentFile)
+                if (currentFile.size > MAX_size) {
+                    return setFileList(prevState => [...prevState, {
+                        file: file,
+                        name: nameFile,
+                        size: sizeFile,
+                        status: ANSWERS.bigSize
+                    }])
+                    if (currentFile.name.length > 15) {
                         return setFileList(prevState => [...prevState, {
                             file: file,
                             name: nameFile,
                             size: sizeFile,
-                            status: ANSWERS.bigSize
+                            status: ANSWERS.bigName
                         }])
-                        if (currentFile.name.length > 15) {
-                            return setFileList(prevState => [...prevState, {
-                                file: file,
-                                name: nameFile,
-                                size: sizeFile,
-                                status: ANSWERS.bigName
-                            }])
-                            if (typeFile !== 'docx') {
-                                return setFileList(prevState => [...prevState, {
-                                    file: file,
-                                    name: nameFile,
-                                    size: sizeFile,
-                                    status: ANSWERS.wrongType
-                                }])
-                            }
-                        }
                     }
-                })
-                console.log(arrFileList)
-            }
-                return setFileList(prevState => [...prevState, {
-                    file: file,
-                    name: nameFile,
-                    size: sizeFile,
-                    status: ANSWERS.success
-                }])
-
-            }
-
-
+                }
+            })
         }
 
 
-        return (
-            <div className="App">
-                <div className="dropzone" id="droptarget">
-                    <input className='dropzone'
-                           type='file'
-                           multiple
-                           value=""
-                           onDrop={handleDrop}/>Drop file here
-                </div>
-                <div>{fileList.length === 0
-                    ? <h1>Files not fined</h1>
-                    : fileList.map((file, index) => (
-                        <div>{index + 1}. <strong>{file.name}</strong>, {file.size} - {file.status}</div>
-                    ))}
-                </div>
-            </div>
-        );
     }
 
 
-    export default App;
+    return (
+        <div className="App">
+            <div className="dropzone" id="droptarget">
+                <input className='dropzone'
+                       type='file'
+                       multiple
+                       value=""
+                       onDrop={handleDrop}/>Drop file here
+            </div>
+            <div>{fileList.length === 0
+                ? <h1>Files not fined</h1>
+                : fileList.map((file, index) => (
+                    <div>{index + 1}. <strong>{file.name}</strong>, {file.size} - {file.status}</div>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+
+export default App;
