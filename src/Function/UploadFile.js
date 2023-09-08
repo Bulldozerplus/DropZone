b21fecc2c12e8f6a2f0d87feee4146274d71cd96
Nikita T <nikitataldykin3156@yandex.ru>
2023-09-08 10:30:39 +0300

    Realized dropZone

--- src/Function/UploadFile.js
+++ src/Function/UploadFile.js
@@ -1,14 +1,14 @@
 import {serverFetch} from "../Servises/URlservices";
 
 export  async function uploadFileFromServer(id, fileName) {
-    const fetchDownloadUrl = await serverFetch.uploadFiles(id)
+    const fetchUploadUrl = await serverFetch.uploadFiles(id)
 
-    if (fetchDownloadUrl.status === 204) {
+    if (fetchUploadUrl.status === 204) {
         setTimeout(() => {
             uploadFileFromServer(id, fileName)
         }, 3000)
     }
-    if (fetchDownloadUrl.status === 200) {
+    if (fetchUploadUrl.status === 200) {
         const blobFetchData = await fetchDownloadUrl.blob()
         let objectURl = URL.createObjectURL(blobFetchData)
         let anchor = document.createElement("a")
